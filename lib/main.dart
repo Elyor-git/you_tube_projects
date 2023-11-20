a421d95ce5b6ab4a127a812922f8afdcd7691b48
elyornorboyev <elyorgit@gmail.com>
2023-11-20 13:54:02 +0500

    you tube video 2 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,21 @@
+
 import 'package:flutter/material.dart';
 
-import 'product_list_widget.dart';
-import 'server.dart';
-import 'shpping_cart_icon.dart';
-
-final GlobalKey<ShoppingCartIconState> shoppingCart =
-GlobalKey<ShoppingCartIconState>();
-final GlobalKey<ProductListWidgetState> productList =
-GlobalKey<ProductListWidgetState>();
+import 'example.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Store',
-      home: MyStorePage(),
-    ),
-  );
+  runApp(MyApp());
 }
 
-class MyStorePage extends StatefulWidget {
-  const MyStorePage({super.key});
-
-  @override
-  MyStorePageState createState() => MyStorePageState();
-}
-
-class MyStorePageState extends State<MyStorePage> {
-  bool _inSearch = false;
-  final TextEditingController _controller = TextEditingController();
-  final FocusNode _focusNode = FocusNode();
-
-  void _toggleSearch() {
-    setState(() {
-      _inSearch = !_inSearch;
-    });
-
-    _controller.clear();
-    productList.currentState!.productList = Server.getProductList();
-  }
-
-  void _handleSearch() {
-    _focusNode.unfocus();
-    final String filter = _controller.text;
-    productList.currentState!.productList =
-        Server.getProductList(filter: filter);
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            leading: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.network('$baseAssetURL/google-logo.png'),
-            ),
-            title: _inSearch
-                ? TextField(
-              autofocus: true,
-              focusNode: _focusNode,
-              controller: _controller,
-              onSubmitted: (_) => _handleSearch(),
-              decoration: InputDecoration(
-                hintText: 'Search Google Store',
-                prefixIcon: IconButton(
-                    icon: const Icon(Icons.search),
-                    onPressed: _handleSearch),
-                suffixIcon: IconButton(
-                    icon: const Icon(Icons.close),
-                    onPressed: _toggleSearch),
-              ),
-            )
-                : null,
-            actions: [
-              if (!_inSearch)
-                IconButton(
-                  onPressed: _toggleSearch,
-                  icon: const Icon(Icons.search, color: Colors.black),
-                ),
-              ShoppingCartIcon(key: shoppingCart),
-            ],
-            backgroundColor: Colors.white,
-            pinned: true,
-          ),
-          SliverToBoxAdapter(
-            child: ProductListWidget(key: productList),
-          ),
-        ],
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: Example(),
     );
   }
 }
